150dd263b65e9ffbecbb3cac12b46dbb99929e86
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-09-23 18:28:09 -0300

    Update and rename ponteiros.c to ponteiros 1.c

--- exercícios/ponteiros 1.c
+++ exercícios/ponteiros 1.c
@@ -1,4 +1,4 @@
-/*  Escreva um programa que declare um inteiro, um real e um char, e ponteiros para inteiro, real, e char. Associe as variáveis aos ponteiros (use &). 
+/*  7. Escreva um programa que declare um inteiro, um real e um char, e ponteiros para inteiro, real, e char. Associe as variáveis aos ponteiros (use &). 
 Modifique os valores de cada variável usando os ponteiros. Imprima os valores das variáveis antes e após a modificação. */
 #include <stdio.h>
 int main(void)
