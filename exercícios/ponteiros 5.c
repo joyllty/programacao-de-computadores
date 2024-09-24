b7a6652caa9e76e397731dc122b1512b477aeb3e
bruna chapelin <161771692+joyllty@users.noreply.github.com>
2024-09-23 22:04:19 -0300

    Update ponteiros 5.c

--- exercícios/ponteiros 5.c
+++ exercícios/ponteiros 5.c
@@ -1,5 +1,5 @@
-/*  16.Crie um programa que contenha uma função que permita passar por parâmetro dois números inteiros A e B. A função devera calcular a soma entre estes 
-dois números e armazenar o resultado na variável A. Esta função não devera possuir retorno, mas devera modificar o valor do primeiro parâmetro. 
+/*  16.Crie um programa que contenha uma função que permita passar por parâmetro dois números inteiros A e B. A função devera calcular a soma entre 
+estes dois números e armazenar o resultado na variável A. Esta função não devera possuir retorn , mas devera modificar o valor do primeiro parâmetro.  
 Imprima os valores de A e B na função principal.*/
 #include <stdio.h>
 void soma(int *a, int b);
@@ -11,11 +11,16 @@ void soma(int *a, int b){
 }
 
 int main(void){
-    int a = 12, b = 15;
+    int a, b ;
+    
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &a);
+    printf("Digite o segundo valor: ");
+    scanf("%d", &b);
+    
+    printf("\nAntes: %d e %d", a, b);
     
     soma(&a, b);
-    
-    printf("Antes: a = 12 e b = 15");
     
     printf("\nDepois: a = %d b= %d", a, b);
 
